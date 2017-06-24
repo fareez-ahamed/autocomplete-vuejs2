94e339ef501a0bd0e2bf485a28bbda77eb40de43
Fareez Ahamed <fareez.ahamed@gmail.com>
2017-06-25 05:09:33 +0530

    Made a working component

--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -1,8 +1,15 @@
 <template>
     <div style="position:relative" v-bind:class="{'open':openSuggestion}">
-        <input class="form-control" type="text" :value="value" @input="updateValue($event.target.value)">
+        <input class="form-control" type="text" :value="value" @input="updateValue($event.target.value)"
+          @keydown.enter = 'enter'
+          @keydown.down = 'down'
+          @keydown.up = 'up'
+        >
         <ul class="dropdown-menu" style="width:100%">
-            <li v-for="suggestion in matches">
+            <li v-for="(suggestion, index) in matches"
+                v-bind:class="{'active': isActive(index)}"
+                @click="suggestionClick(index)"
+            >
               <a href="#">{{ suggestion }}</a>
             </li>
         </ul>
@@ -37,19 +44,58 @@ export default {
     // Filtering the suggestion based on the input
     matches () {
       return this.suggestions.filter((str) => {
-        return str.indexOf(this.selection) >= 0
+        return str.indexOf(this.value) >= 0
       })
     },
 
     openSuggestion () {
-
+      return this.selection !== '' &&
+             this.matches.length !== 0 &&
+             this.open === true
     }
   },
 
   methods: {
+
     updateValue (value) {
+      if (this.open === false) {
+        this.open = true
+        this.current = 0
+      }
       this.$emit('input', value)
+    },
+
+    // When enter pressed on the input
+    enter () {
+      this.$emit('input', this.matches[this.current])
+      this.open = false
+    },
+
+    // When up pressed while suggestions are open
+    up () {
+      if (this.current > 0) {
+        this.current--
+      }
+    },
+
+    // When up pressed while suggestions are open
+    down () {
+      if (this.current < this.matches.length - 1) {
+        this.current++
+      }
+    },
+
+    // For highlighting element
+    isActive (index) {
+      return index === this.current
+    },
+
+    // When one of the suggestion is clicked
+    suggestionClick (index) {
+      this.$emit('input', this.matches[index])
+      this.open = false
     }
+
   }
 
 }
