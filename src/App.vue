0653872426f37a8b5ba469a4e93a1a29d027c841
Fareez Ahamed <fareez.ahamed@gmail.com>
2017-06-24 18:35:56 +0530

    Bismillah

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <autocomplete suggestions="suggestions" v-model="selection"></autocomplete>
+  </div>
+</template>
+
+<script>
+import Autocomplete from './components/Autocomplete'
+
+export default {
+  name: 'app',
+  components: {
+    Autocomplete
+  },
+  data() {
+    return {
+      selection: '',
+      suggestions: ['Chennai', 'Bangalore']
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Autocomplete.vue
@@ -0,0 +1,56 @@
+<template>
+    <div style="position:relative" v-bind:class="{'open':openSuggestion}">
+        <input class="form-control" type="text" :value="value" @input="updateValue($event.target.value)">
+        <ul class="dropdown-menu" style="width:100%">
+            <li v-for="suggestion in matches">
+              <a href="#">{{ suggestion }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+  props: {
+
+    value: {
+      type: String,
+      required: true
+    },
+
+    suggestions: {
+      type: Array,
+      required: true
+    }
+
+  },
+
+  data() {
+    return {
+      open: false,
+      current: 0
+    }
+  },
+
+  computed: {
+    //Filtering the suggestion based on the input
+    matches() {
+      return this.suggestions.filter((str) => {
+        return str.indexOf(this.selection) >= 0
+      });
+    },
+
+    openSuggestion() {
+
+    }
+  },
+
+  methods: {
+    updateValue(value) {
+      this.$emit('input', value)
+    }
+  }
+
+}
+</script>
